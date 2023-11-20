5263cb0390a843495b7f2a1781931ab8d785260b
Dz229 <118302580+Dz229@users.noreply.github.com>
2023-11-20 14:17:03 +0000

    basic structure

---
+++ ur_heuristic.py
@@ -0,0 +1,9 @@
+#####################################################
+# Function calculating value of a Ur board position #
+#####################################################
+
+#This function takes the board list (board_tiles from ur board implementation) and information on tokens that are not on the boad and then returns the value of the board
+def calculate(board, black_tokens_in_home, red_tokens_in_home, black_tokens_finished, red_tokens_finished):
+    value = 0.0
+
+    return value
